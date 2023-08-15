4d4db0c0cc004e8913b85c52d4b32ae535fe2579
jaisachin <138647690+SAZZ-007@users.noreply.github.com>
2023-08-15 13:21:00 +0530

    Create band.py

---
+++ band.py
@@ -0,0 +1,4 @@
+print("welcome to Band Generator")
+city =input( "what's the city you grew up in\n ")
+pet =input( "enter your pet name\n ")
+print( "your band name could be " + city +  " "  + pet )
